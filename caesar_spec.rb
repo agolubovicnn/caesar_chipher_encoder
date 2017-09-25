87deb718b4d5469108b3f77c5d1f344431f0ddac
Aleksandar Golubović <agolubovicnn@gmail.com>
2017-09-25 09:10:36 +0200

    Update caesar_spec.rb

--- caesar_spec.rb
+++ caesar_spec.rb
@@ -1,21 +1,37 @@
 require_relative '../caesar'
 
-RSpec.describe "Caesar" do
+describe Caesar do
+    describe "#caesar_cipher" do
 
-  it "encrypts correctly with alphabet downcase" do
-    expect(Caesar.new(3, "abc").caesar_cipher).to eq("def")
-  end
+      before do
+        cesar = Caesar.new(rotation)
+        cesar.caesar_cipher(string)
+      end
+    end
 
-  it "encrypts correctly with alphabet upcase" do
-    expect(Caesar.new(3, "ABC").caesar_cipher).to eq("DEF")
-  end
+    describe "encrypting" do
+     it "encrypts correctly with alphabet downcase" do
+       expect(Caesar.new(13).caesar_cipher("quick brown fox")).to eq("dhvpx oebja sbk")
+     end
+      
+     it "encrypts correctly with alphabet downcase reverse" do
+       expect(Caesar.new(13).caesar_cipher("dhvpx oebja sbk")).to eq("quick brown fox")
+     end
+      
+     it "encrypts correctly with alphabet upcase" do
+         expect(Caesar.new(3).caesar_cipher("ABC")).to eq("DEF")
+     end
+      
+     it "encrypts correctly with alphabet upcase" do
+       expect(Caesar.new(3).caesar_cipher("ABC")).to eq("DEF")
+     end
 
-  it "encrypts correctly with alphabet capitalized" do
-    expect(Caesar.new(3, "Abc").caesar_cipher).to eq("Def")
-  end
+     it "encrypts correctly with alphabet capitalized" do
+       expect(Caesar.new(26).caesar_cipher("Cool ")).to eq("Cool ")
+     end
 
-  it "encrypts correctly with alphabet camelcase" do
-    expect(Caesar.new(3, "aBc").caesar_cipher).to eq("dEf")
-  end
-
+     it "encrypts correctly with alphabet camelcase" do
+       expect(Caesar.new(3).caesar_cipher("aBc")).to eq("dEf")
+     end
+   end
 end
