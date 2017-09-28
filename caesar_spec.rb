f119196db8c621d2183ea030a118e6fb8eb64e6c
Aleksandar Golubović <agolubovicnn@gmail.com>
2017-09-28 09:58:51 +0200

    Chanded rspec structure

--- caesar_spec.rb
+++ caesar_spec.rb
@@ -1,37 +1,40 @@
 require_relative '../caesar'
 
 describe Caesar do
-    describe "#caesar_cipher" do
 
-      before do
-        cesar = Caesar.new(rotation)
-        cesar.caesar_cipher(string)
+  describe "#caesar_cipher" do
+    context "sentence contain every alphabet letter" do
+      it "returns encrypted string using rotation number 13" do
+        expect(Caesar.new(13).caesar_cipher("The quick brown fox jumps over the lazy dog")).to eq("Gur dhvpx oebja sbk whzcf bire gur ynml qbt")
+      end
+
+      it "returns encrypted string in previous state using rotation number 13" do
+        expect(Caesar.new(13).caesar_cipher("Gur dhvpx oebja sbk whzcf bire gur ynml qbt")).to eq("The quick brown fox jumps over the lazy dog")
       end
     end
 
-    describe "encrypting" do
-     it "encrypts correctly with alphabet downcase" do
-       expect(Caesar.new(13).caesar_cipher("quick brown fox")).to eq("dhvpx oebja sbk")
-     end
-      
-     it "encrypts correctly with alphabet downcase reverse" do
-       expect(Caesar.new(13).caesar_cipher("dhvpx oebja sbk")).to eq("quick brown fox")
-     end
-      
-     it "encrypts correctly with alphabet upcase" do
-         expect(Caesar.new(3).caesar_cipher("ABC")).to eq("DEF")
-     end
-      
-     it "encrypts correctly with alphabet upcase" do
-       expect(Caesar.new(3).caesar_cipher("ABC")).to eq("DEF")
-     end
+    context "word contain alphabet downcase letter" do
+      it "returns encrypted string using rotation number 3" do
+        expect(Caesar.new(3).caesar_cipher("abc")).to eq("def")
+      end
+    end
 
-     it "encrypts correctly with alphabet capitalized" do
-       expect(Caesar.new(26).caesar_cipher("Cool ")).to eq("Cool ")
-     end
+    context "word contain alphabet upcase letter" do
+      it "returns encrypted string using rotation number 3" do
+        expect(Caesar.new(3).caesar_cipher("ABC")).to eq("DEF")
+      end
+    end
 
-     it "encrypts correctly with alphabet camelcase" do
-       expect(Caesar.new(3).caesar_cipher("aBc")).to eq("dEf")
-     end
-   end
+    context "word contain capital alphabet letter" do
+      it "returns encrypted string using rotation number 26" do
+        expect(Caesar.new(26).caesar_cipher("Cool ")).to eq("Cool ")
+      end
+
+      context "word contain camelcase alphabet letter" do
+        it "returns encrypted string using rotation number 3" do
+          expect(Caesar.new(3).caesar_cipher("aBc")).to eq("dEf")
+        end
+      end
+    end
+  end
 end
